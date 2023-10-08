47efb61b9ec3676219b6602c5b637fde968c9987
anshori <m.anshory@gmail.com>
2023-10-08 08:35:13 +0700

    change basemap and decimal coordinate to 7 digit

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 /* Initial Map */
 let tooltip = 'Drag the marker or move the map<br>to change the coordinates<br>of the location';
-let center = [-7.801389645,110.364775452];
+let center = [-7.8013896,110.3647754];
 let map = L.map('map').setView(center, 15); //lat, long, zoom
 map.scrollWheelZoom.disable(); //disable zoom with scroll
 
 /* Tile Basemap */
-let basemap = L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-  maxZoom: 20,
-  subdomains:['mt0','mt1','mt2','mt3'],
-  attribution: 'Google Satellite | <a href="https://unsorry.net" target="_blank">unsorry@2020</a>'
+let basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors | <a href="https://unsorry.net" target="_blank">unsorry@2020</a>'
 });
 basemap.addTo(map);
 
@@ -33,22 +31,22 @@ function dragMarker (e) {
   let latlng = e.target.getLatLng();
 
   //Displays coordinates on the form
-  document.getElementById("latitude").value = latlng.lat.toFixed(5);
-  document.getElementById("longitude").value = latlng.lng.toFixed(5);
+  document.getElementById("latitude").value = latlng.lat.toFixed(7);
+  document.getElementById("longitude").value = latlng.lng.toFixed(7);
 
   //Change the map center based on the marker location
-  map.flyTo(new L.LatLng(latlng.lat.toFixed(9), latlng.lng.toFixed(5)));
+  map.flyTo(new L.LatLng(latlng.lat.toFixed(9), latlng.lng.toFixed(7)));
 }
 
 function mapMovement (e) {
   let mapcenter = map.getCenter();
 
   //Displays coordinates on the form
-  document.getElementById("latitude").value = mapcenter.lat.toFixed(5);
-  document.getElementById("longitude").value = mapcenter.lng.toFixed(5);
+  document.getElementById("latitude").value = mapcenter.lat.toFixed(7);
+  document.getElementById("longitude").value = mapcenter.lng.toFixed(7);
 
   //Create marker
-  marker.setLatLng([mapcenter.lat.toFixed(5), mapcenter.lng.toFixed(5)]).update();
+  marker.setLatLng([mapcenter.lat.toFixed(5), mapcenter.lng.toFixed(7)]).update();
   marker.on('dragend', dragMarker);
 }
 
